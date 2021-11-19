1f7ba09bf4e9167e615e106ee18ab82513663d03
samuel <rcdsamu@gmail.com>
2021-11-19 19:52:02 +0100

    Intento de LogIn

--- app/src/main/java/com/example/ejercicio213/LogIn_Activity.java
+++ app/src/main/java/com/example/ejercicio213/LogIn_Activity.java
@@ -1,7 +1,9 @@
 package com.example.ejercicio213;
 
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,91 +14,84 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
-
-import java.util.HashMap;
-import java.util.Map;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LogIn_Activity extends AppCompatActivity {
 
-    FirebaseFirestore db = FirebaseFirestore.getInstance();
-    CollectionReference users = db.collection("usuarios");
-
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
-
-        EditText username = findViewById(R.id.etmail);
+        mAuth = FirebaseAuth.getInstance();
+        Button btnLogIn = findViewById(R.id.btnLogin);
+        Button btnRegister = findViewById(R.id.btnRegister);
+        EditText email = findViewById(R.id.etmail);
         EditText password = findViewById(R.id.password);
 
-        Button btnLogin = findViewById(R.id.btnLogin);
-        btnLogin.setOnClickListener(new View.OnClickListener() {
+        btnLogIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                db.collection("usuarios")
-                        .whereEqualTo("Username", username.getText().toString())
-                        .get()
-                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                mAuth.signInWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+                        .addOnCompleteListener(LogIn_Activity.this, new OnCompleteListener<AuthResult>() {
                             @Override
-                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                            public void onComplete(@NonNull Task<AuthResult> task) {
                                 if (task.isSuccessful()) {
-                                    for (QueryDocumentSnapshot document : task.getResult()) {
-                                        if (document.get("Password").equals(password.getText().toString())) {
-                                            Intent it = new Intent(LogIn_Activity.this, MainActivity.class);
-                                            startActivity(it);
-                                        }
-                                        break;
-                                    }
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Log.d("TAG", "signInWithEmail:success");
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    updateUI(user);
                                 } else {
-                                    Toast.makeText(getApplicationContext(), "No se ha podido establecer una conexión con la base de datos", Toast.LENGTH_LONG).show();
+                                    // If sign in fails, display a message to the user.
+                                    Log.w("TAG", "signInWithEmail:failure", task.getException());
+                                    Toast.makeText(getApplicationContext(), "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                    updateUI(null);
                                 }
                             }
                         });
             }
         });
 
-        final boolean[] crear = {false};
-        Button btnRegister = findViewById(R.id.btnRegister);
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                DocumentReference docRef = db.collection("usuarios").document(username.getText().toString());
-                docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                        if (task.isSuccessful()) {
-                            DocumentSnapshot document = task.getResult();
-                            if (document.exists()) {
-                                crear[0] = false;
-                            } else {
-                                crear[0] = true;
+            public void onClick(View v) {
+                mAuth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+                        .addOnCompleteListener(LogIn_Activity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Log.d("TAG", "createUserWithEmail:success");
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    updateUI(user);
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.w("TAG", "createUserWithEmail:failure", task.getException());
+                                    Toast.makeText(getApplicationContext(), "Authentication failed.", Toast.LENGTH_SHORT).show();
+                                    updateUI(null);
+                                }
                             }
-                        } else {
-                            Toast.makeText(getApplicationContext(), "No se ha podido establecer una conexión con la base de datos", Toast.LENGTH_LONG).show();
-                            crear[0] = false;
-                        }
-                    }
-                });
-
-                if (crear[0]) {
-                    Toast.makeText(getApplicationContext(), "Usuario creado correctamente", Toast.LENGTH_LONG).show();
-                    Map<String, Object> data1 = new HashMap<>();
-                    data1.put("Username", username.getText().toString());
-                    data1.put("Password", password.getText().toString());
-                    users.document(username.getText().toString()).set(data1);
-                } else {
-                    Toast.makeText(getApplicationContext(), "Ya existe ese usuario", Toast.LENGTH_LONG).show();
-                }
-                username.setText("");
-                password.setText("");
+                        });
             }
         });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            reload();
+        }
+    }
+
+    private void reload() { }
+
+    private void updateUI(FirebaseUser user) {
+
     }
 }
